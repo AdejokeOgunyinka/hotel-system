a38e26d80506e9df8c72b4013af7646ecc8ef576
Adejoke Ogunyinka <ogunyinkajoke@gmail.com>
2021-02-28 02:04:20 +0100

    Fixed model issues

--- db/models/room.py
+++ db/models/room.py
@@ -9,7 +9,7 @@ class Room(models.Model):
     room_type_id = models.ForeignKey('RoomType', on_delete=models.CASCADE)
     room_status_id = models.ForeignKey('RoomStatus', on_delete=models.CASCADE)
     room_no = models.CharField(max_length=255)
-    price = models.CharField(max_length=255, unique=True)
+    price = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
--- db/models/room_type.py
+++ db/models/room_type.py
@@ -4,7 +4,7 @@ from django.db import models
 class RoomType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     type = models.CharField(max_length=255, unique=True)
-    price = models.CharField(max_length=255, unique=True)
+    price = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
